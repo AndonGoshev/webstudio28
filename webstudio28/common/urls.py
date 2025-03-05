b796298512945b2beb4289a660e0dadbe07cec21
andongoshev <andon.go6ev@gmail.com>
2025-03-05 19:07:38 +0200

    about page under construction

--- webstudio28/common/urls.py
+++ webstudio28/common/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webstudio28.common.views import HomeView
+from webstudio28.common.views import HomeView, AboutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
--- webstudio28/common/views.py
+++ webstudio28/common/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'common/home/home.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'common/about/about.html'
